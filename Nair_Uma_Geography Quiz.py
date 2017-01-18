a53ed3daab5ca1a641f110b4cb715516ea7ba569
cvmsprogclub <untitledwon2@gmail.com>
2017-01-18 16:21:26 -0600

    Create Nair_Uma_Geography Quiz.py

---
+++ Nair_Uma_Geography Quiz.py
@@ -0,0 +1,203 @@
+def quiz():
+  print("Here is a quiz to test your knowledge of geography...")
+  print()
+  print()
+  print("Question #1")
+  print("What is the capital of California?")
+  print()
+  print("a. Sacramento")
+  print("b. San Francisco")
+  print("c. Los Angeles")
+  answer = input("Make your choice: ")
+  if answer [0]. lower () == "a":
+    print("Correct! Sacramento is the capital of California.")
+  elif answer [0]. lower () == "b": 
+    print("Incorrect! Sacramento is the capital of California.")
+  elif answer [0]. lower () == "c": 
+    print("Incorrect! Sacramento is the capital of California.")
+  else:
+    print("Enter a or b or c")
+
+def quiz_1():    
+  print()
+  print()
+  print("Question #2")
+  print("How many oceans border North America?")
+  print()
+  print("a. 1")
+  print("b. 2")
+  print("c. 3")
+  answer = input("Make your choice: ")
+  if answer [0]. lower () == "a":
+    print("Incorrect! North America is surrounded by 3 oceans.")
+  elif answer [0]. lower () == "b": 
+    print("Incorrect! North America is surrounded by 3 oceans.")
+  elif answer [0]. lower () == "c": 
+    print("Correct! North America is surrounded by 3 oceans.")
+  else:
+    print("Enter a or b or c")
+
+def quiz_2():    
+  print()
+  print()
+  print("Question #3")
+  print("North America is surrounded by which of the following oceans? ")
+  print()
+  print("a. Indian, Pacific,and Arctic Oceans")
+  print("b. Southern, Arctic, and Pacific Oceans")
+  print("c. Pacific, Atlantic, and Arctic Oceans")
+  answer = input("Make your choice: ")
+  if answer [0]. lower () == "a":
+    print("Incorrect! North America is surrounded by the Pacific, Atlantic, and Arctic Oceans.")
+  elif answer [0]. lower () == "b": 
+    print("Incorrect! North America is surrounded by the Pacific, Atlantic, and Arctic Oceans.")
+  elif answer [0]. lower () == "c": 
+    print("Correct! North America is surrounded by the Pacific, Atlantic, and Arctic Oceans.")
+  else:
+    print("Enter a or b or c")
+
+def quiz_3():
+  print()
+  print()
+  print("Question #4")
+  print("What is the capital of Texas?")
+  print()
+  print("a. Houston")
+  print("b. Dallas")
+  print("c. Austin")
+  answer = input("Make your choice: ")
+  if answer [0]. lower () == "a":
+    print("Incorrect! Austin is the capital of Texas.")
+  elif answer [0]. lower () == "b": 
+    print("Incorrect! Austin is the capital of Texas.")
+  elif answer [0]. lower () == "c": 
+    print("Correct! Austin is the capital of Texas.")
+  else:
+    print("Enter a or b or c")
+
+def quiz_4():    
+  print()
+  print()
+  print("Question #5")
+  print("How many countries are in South America?")
+  print()
+  print("a. 10")
+  print("b. 12")
+  print("c. 15")
+  answer = input("Make your choice: ")
+  if answer [0]. lower () == "a":
+    print("Incorrect! There are 12 countries in South America.")
+  elif answer [0]. lower () == "b":  
+    print("Correct! There are 12 countries in South America.")
+  elif answer [0]. lower () == "c": 
+    print("Incorrect! There are 12 countries in South America.")
+  else:
+    print("Enter a or b or c")
+
+def quiz_5():    
+  print()
+  print()
+  print("Question #6")
+  print("Where is MIT, a private research university, located?")
+  print()
+  print("a. Mississippi")
+  print("b. Massachusetts")
+  print("c. Mexico")
+  answer = input("Make your choice: ")
+  if answer [0]. lower () == "a":
+    print("Incorrect! MIT is located in Massachusetts.")
+  elif answer [0]. lower () == "b": 
+    print("Correct! MIT is located in Massachusetts.")
+  elif answer [0]. lower () == "c": 
+    print("Incorrect! MIT is located in Massachusetts.")
+  else:
+    print("Enter a or b or c")
+
+def quiz_6():
+  print()
+  print()
+  print("Question #7")
+  print("How many states are in Africa?")
+  print()
+  print("a. 36")
+  print("b. 40")
+  print("c. 54")
+  answer = input("Make your choice: ")
+  if answer [0]. lower () == "a":
+    print("Incorrect! There are 54 states in Africa.")
+  elif answer [0]. lower () == "b": 
+    print("Incorrect! There are 54 states in Africa.")
+  elif answer [0]. lower () == "c": 
+    print("Correct! There are 54 states in Africa.")
+  else:
+    print("Enter a or b or c")
+
+def quiz_7():
+  print()
+  print()
+  print("Question #8")
+  print("Which of the following states is located in Africa?")
+  print()
+  print("a. Ethiopia")
+  print("b. Ontario")
+  print("c. New York")
+  answer = input("Make your choice: ")
+  if answer [0]. lower () == "a":
+    print("Correct! Ethiopia is a state located in Africa.")
+  elif answer [0]. lower () == "b": 
+    print("Incorrect! Ethiopia is a state located in Africa.")
+  elif answer [0]. lower () == "c": 
+    print("Incorrect! Ethiopia is a state located in Africa.")
+  else:
+    print("Enter a or b or c")
+
+def quiz_8(): 
+  print()
+  print()
+  print("Question #9")
+  print("What is the capital of Tennesee?")
+  print()
+  print("a. Nashville")
+  print("b. Memphis")
+  print("c. Clarksville")
+  answer = input("Make your choice: ")
+  if answer [0]. lower () == "a":
+    print("Correct! Nashville is the capital of Tennesee.")
+  elif answer [0]. lower () == "b": 
+    print("Incorrect! Nashville is the capital of Tennesee.")
+  elif answer [0]. lower () == "c": 
+    print("Incorrect! Nashville is the capital of Tennesee.")
+  else:
+    print("Enter a or b or c")
+
+def quiz_9():  
+  print()
+  print()
+  print("Question #10")
+  print("Where is the city of Gatlinburg located?")
+  print()
+  print("a. Oregon")
+  print("b. Tennesee")
+  print("c. Idaho")
+  answer = input("Make your choice: ")
+  if answer [0]. lower () == "a":
+    print("Incorrect! The city of Gatlinburg is located in Tennesee.")
+  elif answer [0]. lower () == "b": 
+    print("Correct! The city of Gatlinburg is located in Tennesee.")
+  elif answer [0]. lower () == "c": 
+    print("Incorrect! The city of Gatlinburg is located in Tennesee.")
+  else:
+    print("Enter a or b or c")
+    print()
+    print("Thank you for taking my quiz!")
+
+quiz()
+quiz_1()
+quiz_2()
+quiz_3()
+quiz_4()
+quiz_5()
+quiz_6()
+quiz_7()
+quiz_8()
+quiz_9()
