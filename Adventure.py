221c64a43227d1a8670e981a28d83c4de5669526
cvmsprogclub <untitledwon2@gmail.com>
2017-01-18 16:22:06 -0600

    Add files via upload

---
+++ Adventure.py
@@ -0,0 +1,25 @@
+print("You're stranded in a desert")
+print("press a to jump off a cliff")
+print("press b to search for water")
+print("press c to search for food")
+user_choice = input()
+if user_choice == "a" :
+    print("You jumped off a cliff and died.")
+elif user_choice == "b" :
+    print("you found a pond")
+    print("Do you want to drink it? (y/n)")
+    user_choice = input()
+    if user_choice =="y" :
+         print("you drank the water and aren't thirsty anymore.")
+         print("You fin an old man riding on a camel. He asks you what's 9+10. n=21 m=19(n/m)"
+    if user_choice == "n" :
+               print("He says, you are clearly not asian. I hate non-asians. HE KILLS YOU."
+    elif user_choice
+
+    elif user_choice == "n":
+       print("YOU DIED of deydration")
+
+elif user_choice == "c":
+     print("You found a dead animal, but while you tried to eat it you got poisoned, and YOU DIED")
+    
+    
