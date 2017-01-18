e97a758e35a412a126acbfa40d205703e9185791
cvmsprogclub <untitledwon2@gmail.com>
2017-01-18 16:26:35 -0600

    Add files via upload

---
+++ Aatmo the Batamo's adventure.py
@@ -0,0 +1,7 @@
+print("Hello, you are in an island of the coast of Mesopotamia, and are badly injured, you have to figure out how to live(or die)ಠ⌣ಠ")
+x = input("You have found some red berries, do you want to eat them, type'y' for yes, and 'n' for no")
+if x[0].lower() == 'y':
+    print("Sorry, the berries were poisonous, you died")
+elif x[0].lower() == 'n':
+    print("Good, the berries were poisonous")
+else: print(79719479714971894769754681763497698746197**8012870871708570487397018470801470)
