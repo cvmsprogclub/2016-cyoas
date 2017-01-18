7b157c48139977593439b46745b4a1f9f44fda27
cvmsprogclub <untitledwon2@gmail.com>
2017-01-18 16:19:10 -0600

    Add files via upload

---
+++ Test v0.1.py
@@ -0,0 +1,13 @@
+print("This is THE TEST v0.1")
+user1 = input("Do you wish to begin? (y/n)")
+if user1[0] == "y":
+    print("THE TEST BEGINS!!!!!!!")
+    points = 1
+elif user1[0] == "n":
+    print("www.xkcd.com")
+    print("FOLLOW THIS LINK!!!!!!!!")
+    print("YOU GOT A -infinity")
+    quit()
+ans1 = input("What base replaces thymine in RNA?(no capitals) ")
+if ans1.lower() == "uracil" :
+    print("yay")
